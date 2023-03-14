a2b80669182251a85a142027190fd136927c7b93
Pietro Pugliesi <pietropugliesi@icloud.com>
2023-03-14 13:21:13 -0300

    add vim, emacs, bash and zsh

---
+++ private_dot_emacs.d/transient/history.el
@@ -0,0 +1,13 @@
+((magit-branch nil)
+ (magit-commit nil
+	       ("--all"))
+ (magit-dispatch nil)
+ (magit-fetch nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate")
+  ("-n256" "--simplify-by-decoration" "--graph" "--decorate"))
+ (magit-pull nil)
+ (magit-push nil
+	     ("--force-with-lease"))
+ (magit-run nil)
+ (magit-status-jump nil))
